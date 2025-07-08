04f0114e095a4c19d47b142535a90fd198537e86
Marco Nabacino <10609636@polimi.it>
2025-07-08 12:26:04 +0200

    Added save_g2.py to compute and save autocorrelation curves for Trajector-Age measurements taken with Flhemy

---
+++ t-age/save_g2.py
@@ -0,0 +1,60 @@
+import utils.data_loaders as data_loaders
+from utils.timetagger import get_correlator_architecture
+import pandas as pd
+import os
+import numpy as np
+
+# Input folder
+DCS_root = "W:/DATA/fNIRS_LAB/TRAJECTOR_AGE/Parma_Metaboliche/DCS/"
+
+# Read general info file.
+info_file = "C:/Users/marco/OneDrive - Politecnico di Milano/Dottorato/Trajector-Age/Database_misure.xlsx"
+info = pd.read_excel(info_file)
+# Find indices of measurements to process based on "City" column
+mask = (info["City"] == "PV") & (info["Eligible DCS"] == "Yes")
+idx_measurements_to_process = info.index[mask].tolist()
+idx_measurements_to_process = idx_measurements_to_process[0:1] # For testing purposes, process only the first measurement
+
+# Process each measurement
+for i_meas in idx_measurements_to_process:
+    subject = info.loc[i_meas, "Subject"]
+    time_point = info.loc[i_meas, "TimePoint"]
+    exercise = info.loc[i_meas, "Exercise"]
+    print(f"Processing measurement {i_meas} ({subject}_{time_point}_{exercise})...")
+
+    # Get list of DCS files
+    DCS_folder = info.loc[i_meas, "DCS Folder"]
+    DCS_path = os.path.join(DCS_root, DCS_folder)
+    DCS_name = info.loc[i_meas, "DCS Name"]
+    DCS_data_file = DCS_path + DCS_name + ".ttbin"
+    print(f"Loading DCS data from {DCS_data_file}...")
+    # Load DCS data
+    m = 2
+    (p, s) = get_correlator_architecture(alpha=7, m=m, tau_max=1e-2, t0=1e-12)
+    integration_time = 1 / info.loc[i_meas, "Sample Frequency"]
+    tau_start = 1e-7
+    loader = data_loaders.DataLoaderTimeTagger(
+        DCS_data_file,
+        integration_time=integration_time,
+        channels=[1, 2, 3, 4],
+        p=p,
+        m=m,
+        s=s,
+        tau_start=tau_start,
+    )
+    loader.load_data(plot_interval=100)
+
+    # Discard noisy tau channels
+    mask = loader.tau > tau_start
+    tau = loader.tau[mask]
+    g2_norm = loader.g2_norm[mask, ...]
+    countrate = loader.countrate
+
+    # Save autocorrelation data to .npz file
+    output_folder = DCS_path
+    output_file_name = DCS_name
+    output_file = os.path.join(output_folder, output_file_name)
+    print(f"Saving autocorrelation data to {output_file}...")
+    np.savez(output_file, tau=tau, g2_norm=g2_norm, countrate=countrate)
+
+    print("Done.")

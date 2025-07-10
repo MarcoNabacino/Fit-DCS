Added parallelepiped WIP, there's a bug in the calculation of ds

forward/homogeneous_inf.py
@@ -15,8 +15,7 @@ def g1(msd: np.ndarray, mua: float, musp: float, r: float, lambda0: float) -> np
     :param lambda0: Wavelength of the light source. [nm]
     :return: The unnormalized first-order autocorrelation function g1. A vector the same length as tau.
     """
-    lambda0 *= 1e-7 # Convert to cm
-    k0 = 2 * np.pi / lambda0
+    k0 = 2 * np.pi / (lambda0 * 1e-7) # Convert lambda0 to cm
     k = np.sqrt(3 * musp * mua + musp**2 * k0**2 * msd)
 
     return np.exp(-k * r) / r
@@ -36,7 +35,8 @@ def g1_norm(msd: np.ndarray | float, mua: float, musp: float, r: float, lambda0:
     return g1(msd, mua, musp, r, lambda0) / g1(0, mua, musp, r, lambda0)
 
 
-def d_factors(msd0: np.ndarray, mua0: float, musp0: float, r: float, lambda0: float) -> tuple:
+def d_factors(msd0: np.ndarray, mua0: float, musp0: float, r: float, lambda0: float) \
+        -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Calculates the d factors for the DCS Modified Beer-Lambert law for the homogeneous infinite medium. See [1] for
     an explanation.
@@ -52,8 +52,7 @@ def d_factors(msd0: np.ndarray, mua0: float, musp0: float, r: float, lambda0: fl
         msd, da is the d factor for mua, and ds is the d factor for musp. To get the d factor for the dynamical
         parameter of interest (i.e., Db or v_ms), multiply dr by d(msd)/d(Db) or d(msd)/d(v_ms), respectively.
     """
-    lambda0 *= 1e-7 # Convert to cm
-    k0 = 2 * np.pi / lambda0
+    k0 = 2 * np.pi / (lambda0 * 1e-7) # Convert lambda0 to cm
 
     k = np.sqrt(3 * musp0 * mua0 + musp0**2 * k0**2 * msd0)
     mu_eff = np.sqrt(3 * mua0 * musp0)
@@ -63,3 +62,35 @@ def d_factors(msd0: np.ndarray, mua0: float, musp0: float, r: float, lambda0: fl
     ds = r * ((3 * mua0 + 2 * k0**2 * musp0 * msd0) / k - 3 * mua0 / mu_eff)
 
     return dr, da, ds
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import common
+
+    tau = np.logspace(-6, -2, 200)
+    db = 1e-8
+    msd = common.msd_brownian(tau, db)
+    mua = 0.1
+    musp = 10
+    r = 2
+    lambda0 = 785
+
+    (dr, da, ds) = d_factors(msd, mua, musp, r, lambda0)
+    ddb = dr * common.d_msd_brownian(tau)
+
+    # Plot ddb on right axis, da and ds on left axis
+    fig, ax1 = plt.subplots()
+
+    ax1.semilogx(tau, da, "--", label=r"$d_a$")
+    ax1.semilogx(tau, ds, "--", label=r"$d_s$")
+    ax1.set_xlabel(r"$\tau$ (s)")
+    ax1.set_ylabel(r"$d_a, d_s$ (cm)")
+
+    ax2 = ax1.twinx()
+    ax2.semilogx(tau, ddb * 1e-8, label=r"$d_{Db}$", color='tab:green')
+    ax2.set_ylabel(r"$d_{Db} \times 10^{-8}$ (s/cm$^2$)")
+    ax1.legend(loc="upper left")
+    ax2.legend(loc="upper right")
+
+    plt.title("Sensitivity factors for homogeneous infinite medium")
+    plt.show()

forward/homogeneous_inf_slab.py
@@ -119,7 +119,7 @@ def g1_reflectance_norm(msd: np.ndarray, mua: float, musp: float, rho: float, n:
 
 
 def d_factors_transmittance(msd0: np.ndarray, mua0: float, musp0: float, rho: float, n: float, lambda0: float,
-                            d: float, m_max: int) -> tuple:
+                            d: float, m_max: int) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Calculates the d factors for the DCS Modified Beer-Lambert law for the laterally infinite slab in transmittance.
     See [1] for an explanation.
@@ -145,7 +145,7 @@ def d_factors_transmittance(msd0: np.ndarray, mua0: float, musp0: float, rho: fl
     a = common.a_coefficient_boundary(n)
     zb = 2 * a / (3 * musp0)
     k = np.sqrt(3 * musp0 * mua0 + musp0**2 * k0**2 * msd0)
-    dk = (3 * mua0 + musp0 * k0**2 * msd0) / (2 * k) # Derivative of k with respect to musp
+    dk = (3 * mua0 + 2 * musp0 * k0**2 * msd0) / (2 * k) # Derivative of k with respect to musp
     mu_eff = np.sqrt(3 * mua0 * musp0)
     dmu_eff = 3 * mua0 / (2 * mu_eff) # Derivative of mu_eff with respect to musp
 
@@ -205,7 +205,7 @@ def d_factors_transmittance(msd0: np.ndarray, mua0: float, musp0: float, rho: fl
 
 
 def d_factors_reflectance(msd0: np.ndarray, mua0: float, musp0: float, rho: float, n: float, lambda0: float,
-                          d: float, m_max: int) -> tuple:
+                          d: float, m_max: int) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Calculates the d factors for the DCS Modified Beer-Lambert law for the laterally infinite slab in reflectance.
     See [1] for an explanation.
@@ -231,7 +231,7 @@ def d_factors_reflectance(msd0: np.ndarray, mua0: float, musp0: float, rho: floa
     a = common.a_coefficient_boundary(n)
     zb = 2 * a / (3 * musp0)
     k = np.sqrt(3 * musp0 * mua0 + musp0 ** 2 * k0 ** 2 * msd0)
-    dk = (3 * mua0 + musp0 * k0 ** 2 * msd0) / (2 * k)  # Derivative of k with respect to musp
+    dk = (3 * mua0 + 2 * musp0 * k0 ** 2 * msd0) / (2 * k)  # Derivative of k with respect to musp
     mu_eff = np.sqrt(3 * mua0 * musp0)
     dmu_eff = 3 * mua0 / (2 * mu_eff)  # Derivative of mu_eff with respect to musp
 
@@ -288,3 +288,37 @@ def d_factors_reflectance(msd0: np.ndarray, mua0: float, musp0: float, rho: floa
     ds = - 2 / den * (a1 - a2) + 2 / den0 * (a1_0 - a2_0)
 
     return dr, da, ds
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    tau = np.logspace(-6, -2, 200)
+    db = 1e-8
+    msd = common.msd_brownian(tau, db)
+    mua = 0.1
+    musp = 10
+    rho = 0
+    n = 1.4
+    lambda0 = 785
+    d = 1.0
+    m_max = 10
+
+    (dr, da, ds) = d_factors_transmittance(msd, mua, musp, rho, n, lambda0, d, m_max)
+    ddb = dr * common.d_msd_brownian(tau)
+
+    # Plot ddb on right axis, da and ds on left axis
+    fig, ax1 = plt.subplots()
+
+    ax1.semilogx(tau, da, "--", label=r"$d_a$")
+    ax1.semilogx(tau, ds, "--", label=r"$d_s$")
+    ax1.set_xlabel(r"$\tau$ (s)")
+    ax1.set_ylabel(r"$d_a, d_s$ (cm)")
+
+    ax2 = ax1.twinx()
+    ax2.semilogx(tau, ddb * 1e-8, label=r"$d_{Db}$", color='tab:green')
+    ax2.set_ylabel(r"$d_{Db} \times 10^{-8}$ (s/cm$^2$)")
+    ax1.legend(loc="upper left")
+    ax2.legend(loc="upper right")
+
+    plt.title("Sensitivity factors for homogeneous laterally infinite slab (transmittance)")
+    plt.show()

forward/homogeneous_semi_inf.py
@@ -49,10 +49,11 @@ def g1_norm(msd: np.ndarray | float, mua: float, musp: float, rho: float, n: flo
     return g1(msd, mua, musp, rho, n, lambda0) / g1(0, mua, musp, rho, n, lambda0)
 
 
-def d_factors(msd0: np.ndarray, mua0: float, musp0: float, rho: float, n: float, lambda0: float) -> tuple:
+def d_factors(msd0: np.ndarray, mua0: float, musp0: float, rho: float, n: float, lambda0: float) \
+        -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
-    Calculates the d factors for the DCS Modified Beer-Lambert law for the homogeneous semi-infinite medium. See [1] for
-    an explanation.
+    Calculates the d factors for the DCS Modified Beer-Lambert law for the homogeneous semi-infinite medium.
+    See [1] for an explanation.
 
     [1] Baker, W. et al. (2014), "Modified Beer-Lambert law for blood flow"
     
@@ -63,9 +64,10 @@ def d_factors(msd0: np.ndarray, mua0: float, musp0: float, rho: float, n: float,
     :param n: Ratio of the refractive index of the medium to the refractive index of the surrounding medium
         (typically air).
     :param lambda0: Wavelength of the light source. [nm]
-    :return: The d factors. A tuple of three vectors (dr, da, ds), each the same length as tau. dr is the d factor for
-        msd, da is the d factor for mua, and ds is the d factor for musp. To get the d factor for the dynamical
-        parameter of interest (i.e., Db or v_ms), multiply dr by d(msd)/d(Db) or d(msd)/d(v_ms), respectively.
+    :return: The d factors. A tuple of three vectors (dr, da, ds), each the same length as tau.
+        dr is the d factor for msd, da is the d factor for mua, and ds is the d factor for musp.
+        To get the d factor for the dynamical parameter of interest (i.e., Db or v_ms), multiply dr by
+        d(msd)/d(Db) or d(msd)/d(v_ms), respectively.
     """
     lambda0 *= 1e-7 # Convert to cm
     k0 = 2 * np.pi / lambda0
@@ -108,3 +110,35 @@ def d_factors(msd0: np.ndarray, mua0: float, musp0: float, rho: float, n: float,
     ds = 2 * (num / den - num0 / den0)
 
     return dr, da, ds
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    tau = np.logspace(-6, -2, 200)
+    db = 1e-8
+    msd = common.msd_brownian(tau, db)
+    mua = 0.1
+    musp = 10
+    rho = 2
+    n = 1.4
+    lambda0 = 785
+
+    (dr, da, ds) = d_factors(msd, mua, musp, rho, n, lambda0)
+    ddb = dr * common.d_msd_brownian(tau)
+
+    # Plot ddb on right axis, da and ds on left axis
+    fig, ax1 = plt.subplots()
+
+    ax1.semilogx(tau, da, "--", label=r"$d_a$")
+    ax1.semilogx(tau, ds, "--", label=r"$d_s$")
+    ax1.set_xlabel(r"$\tau$ (s)")
+    ax1.set_ylabel(r"$d_a, d_s$ (cm)")
+
+    ax2 = ax1.twinx()
+    ax2.semilogx(tau, ddb * 1e-8, label=r"$d_{Db}$", color='tab:green')
+    ax2.set_ylabel(r"$d_{Db} \times 10^{-8}$ (s/cm$^2$)")
+    ax1.legend(loc="upper left")
+    ax2.legend(loc="upper right")
+
+    plt.title("Sensitivity factors for homogeneous semi-infinite medium")
+    plt.show()